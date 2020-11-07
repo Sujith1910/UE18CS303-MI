a43e387f9748d6e228feffcc08ba3fedf84f46f9
Sujith K <sujithk1910@gmail.com>
2020-11-07 12:06:52 +0530

    added assignment3 files

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -213,6 +213,8 @@ def dfs_recursive(node, cost, goals, visited):
 adding random comments for plagiarism checker.
 random random. check dfs hi hih 2332
 test file is passing. we could have tried bfs also.
+fun fun fun. test arsh 12 34
+is it working 
 '''
 
 def DFS_Traversal(cost, start_point, goals):
@@ -239,4 +241,52 @@ def tri_Traversal(cost, heuristic, start_point, goals):
     l.append(t3)
 
     return l
+
+
+def test_case():
+    '''size of cost matrix is 11x11
+    0th row and 0th column is ALWAYS 0
+    Number of nodes is 10
+    size of heuristic list is 11
+    0th index is always 0'''
+
+    cost = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 5, 9, -1, 6, -1, -1, -1, -1, -1],
+            [0, -1, 0, 3, -1, -1, 9, -1, -1, -1, -1],
+            [0, -1, 2, 0, 1, -1, -1, -1, -1, -1, -1],
+            [0, 6, -1, -1, 0, -1, -1, 5, 7, -1, -1],
+            [0, -1, -1, -1, 2, 0, -1, -1, -1, 2, -1],
+            [0, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1],
+            [0, -1, -1, -1, -1, -1, -1, 0, -1, -1, -1],
+            [0, -1, -1, -1, -1, 2, -1, -1, 0, -1, 8],
+            [0, -1, -1, -1, -1, -1, -1, -1, -1, 0, 7],
+            [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0]]
+    heuristic = [0, 5, 7, 3, 4, 6, 0, 0, 6, 5, 0]
+
+    try:
+        # start node is 1, goal states to reach are 6,7 & 10
+        if (tri_Traversal(cost, heuristic, 1, [6, 7, 10])[0] == [1, 2, 3, 4, 7]):
+            print("SAMPLE TEST CASE 1 FOR THE  DFS_TRAVERSAL PASSED")
+        else:
+            print("SAMPLE TEST CASE 1 FOR THE  DFS_TRAVERSAL FAILED")
+    except:
+        print("SAMPLE TEST CASE 1 FOR THE DFS_TRAVERSAL FAILED")
+    try:
+        if (tri_Traversal(cost, heuristic, 1, [6, 7, 10]))[1] == [1, 5, 4, 7]:
+            print("SAMPLE TEST CASE 2 FOR THE  UCS_TRAVERSAL PASSED")
+        else:
+            print("SAMPLE TEST CASE 2 FOR THE  UCS_TRAVERSAL FAILED")
+    except:
+        print("SAMPLE TEST CASE 2 FOR THE UCS_TRAVERSAL FAILED")
+
+    try:
+        if (tri_Traversal(cost, heuristic, 1, [6, 7, 10]))[2] == [1, 5, 4, 7]:
+            print("SAMPLE TEST CASE 3 FOR THE  A_star_TRAVERSAL PASSED")
+        else:
+            print("SAMPLE TEST CASE 3 FOR THE  A_star_TRAVERSAL FAILED")
+    except:
+        print("SAMPLE TEST CASE 3 FOR THE A_star_TRAVERSAL FAILED")
+
+
+test_case()
 
